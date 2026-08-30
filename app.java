dd647034b544ee20c73c5e8ab0cc17dae64d9875
hari <hari@gmail.com>
2026-08-30 16:08:03 +0000

    java file data added

---
+++ app.java
@@ -0,0 +1,7 @@
+// Saved in a file named Main.java
+public class Main {
+    public static void main(String[] args) {
+        // Prints the text to the console
+        System.out.println("Hello, World!");
+    }
+}
